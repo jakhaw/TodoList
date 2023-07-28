ddb7767636f9039ece93cdfb57f73ed85494040d
Jakub Hawryłkowicz <hawrylkowicz.jakub@gmail.com>
2023-07-28 17:09:26 +0200

    Creating and handling a form that allows user to add new tasks, listing existing tasks

--- src/Controller/ListController.php
+++ src/Controller/ListController.php
@@ -2,17 +2,51 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
+use App\Form\TaskFormType;
+use App\Repository\TaskRepository;
+use App\Repository\TodoListRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ListController extends AbstractController
 {
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private TaskRepository $taskRepository,
+        private TodoListRepository $todoListRepository,
+    ) {
+    }
+
     #[Route('/list/{id}', name: 'app_list')]
-    public function index($id): Response
+    public function index($id, Request $request): Response
     {
+        $todolist = $this->todoListRepository->findOneBy(['id' => $id]);
+        $tasks = $todolist->getTasks();
+
+        $task = new Task();
+        $form = $this->createForm(TaskFormType::class, $task);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+
+            $newTask = $form->getData();
+
+            $newTask->setTodolist($todolist);
+
+            $this->entityManager->persist($newTask);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('app_list', ['id' => $id]);
+        };
+
         return $this->render('list/index.html.twig', [
-            'controller_name' => 'ListController',
+            'tasks' => $tasks,
+            'todolist' => $todolist,
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/TaskFormType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Task;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Description', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control form-control-lg me-4',
+                    'placeholder' => 'New Task...',
+                ],
+                'label' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Task::class,
+        ]);
+    }
+}
